373e92e6460dade6092ea33bd46c3bd9fe6c8071
jonibarmak <jonathan.barmak95@gmail.com>
2022-08-05 21:52:01 -0300

    funcionando

--- products/views.py
+++ products/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import redirect, render
 from products.models import Products 
 from products.forms import Formulario_productos 
 
-
 def create_product(request):
-     if request.method=="POST":
+    if request.method=="POST":
         form=Formulario_productos(request.POST)
         if form.is_valid():
             Products.objects.create(
@@ -16,7 +15,7 @@ def create_product(request):
             )
             return redirect(list_products)
 
-     elif request.method=="GET":
+    elif request.method=="GET":
         form=Formulario_productos()
         context={"form":form}
         return render(request,"new_product.html",context=context)
@@ -29,8 +28,18 @@ def list_products(request):
     return render(request,"products_list.html",context=context)
 
 def primer_formulario(request):
+    print(request.method)
     if request.method =="POST":
         Products.objects.create(name=request.POST["name"])
     return render(request,"primer_formulario.html",context={})
+
+def search_products(request):
+    search=request.GET["search"]
+    products=Products.objects.filter(name__icontains=search)
+    context={"products":products}
+    return render(request,"search_product.html",context=context)
+
+
+
 
 
