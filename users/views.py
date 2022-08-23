e58b58b564ec7128ee1ff5b91d791774d92f58c8
jonibarmak <jonathan.barmak95@gmail.com>
2022-08-22 23:12:32 -0300

    editar perfil

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
-from users.forms import User_registration_form, Profile_Form
+from users.forms import User_registration_form
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import User_profile 
+
 
 
 def login_request(request):
@@ -47,6 +47,24 @@ def register(request):
 
 def show_profile(request):
     return HttpResponse(request.user.profile.name)
+
+@login_required
+def edit_profile(request):
+    user=request.user
+    if request.method == "POST":
+        form=User_registration_form(request.POST)
+        if form.is_valid():
+            user.email=form.cleaned_data["email"]
+            user.password1=form.cleaned_data["password1"]
+            user.password2=form.cleaned_data["password2"]
+            user.save()
+        return render(request, "index.html")
+    else:
+        form=User_registration_form(initial={
+            "username":user.username,
+            "email":user.email})
+        return render(request,"edit_profile.html",{"form":form,"user":user})
+
 
 
 
