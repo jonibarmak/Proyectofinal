47bb9be37c795a91698b92c39e8c16a5f99c9c40
Fosktrot <109191710+Fosktrot@users.noreply.github.com>
2022-08-27 18:25:35 -0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,15 +1,15 @@
 from tkinter import CASCADE
 from django.db import models
 
-class Products(models.Model):
-    name= models.CharField(max_length=40)
-    price= models.FloatField()
-    description= models.CharField(max_length=200, null=True, blank=True)
-    is_active=models.BooleanField(default=True)
-    creation_date=models.DateField(auto_now_add=True,null=True,blank=True)
-    stock=models.IntegerField()
-    image=models.ImageField(upload_to="products/",null=True,blank=True)
-    brand=models.CharField(max_length=40,null=True, blank=True)
+class Products(models.Model):                                                   #modelo para cada zapatilla
+    name= models.CharField(max_length=40)                                       #nombre de la zapa
+    price= models.FloatField()                                                  #precio de la zapa
+    description= models.CharField(max_length=200, null=True, blank=True)        #descripcion de la zapa
+    is_active=models.BooleanField(default=True)                     
+    creation_date=models.DateField(auto_now_add=True,null=True,blank=True)  
+    stock=models.IntegerField()                                                 #cantidad de zapas 
+    image=models.ImageField(upload_to="products/",null=True,blank=True)         #imagen de la zapa        
+    brand=models.CharField(max_length=40,null=True, blank=True)                 #marca
 
     def __str__(self): 
         return self.name 
