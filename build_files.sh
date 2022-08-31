d0e69189b7af2458ba70c0d5962f7555a1ee2c06
jonibarmak <109324736+jonibarmak@users.noreply.github.com>
2022-08-30 23:58:05 -0300

    Delete build_files.sh

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-# Update pip
-python3.9 -m pip install --upgrade pip
-
-# Install requirements
-pip install -r requirements.txt
-
-# Collect Static Files on Deploy
-python3.9 manage.py collectstatic
